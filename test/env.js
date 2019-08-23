1a9f1e40ca9279144bb7a597d41a8d1ba855b52b
John Internet <jonjon1234.github@gmail.com>
2019-08-23 08:44:51 +0100

    Added test cases for provided env.

--- test/env.js
+++ test/env.js
@@ -17,5 +17,30 @@ describe("EnvDBHandler", function(){
 				"NODE_ENV": "test"
 			})
 		})
+		it("should construct and merge when passed an env object with DB values.", function(){
+			let x = new db({
+				"NODE_ENV": "test",
+				"DB_AUTO": "API",
+				"DB_HOST": "nigeria",
+				"DB_USER": "big_money_prince",
+				"DB_PORT": "3307",
+				"DB_API_DB": "money",
+				"DB_API_PASS": "mysecurepassword",
+				"DB_API_USER": ""
+			})
+			assert.deepStrictEqual(x.configs, {
+				default: {
+					host: 'nigeria',
+					user: 'big_money_prince',
+					port: 3307
+				},
+				api: {
+					host: 'nigeria',
+					port: 3307,
+					password: 'mysecurepassword',
+					database: 'money'
+				}
+			})
+		})
 	})
 })
