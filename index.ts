0b2e23ad4fa790eda9af08ee356c46e22f735a46
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-09-25 01:59:51 +0100

    refactor: Rebuild with ts files.

---
+++ index.ts
@@ -0,0 +1,5 @@
+import InterDBError from "./lib/InterDBError"
+import ListDBHandler from "./lib/ListDBHandler"
+export {InterDBError, ListDBHandler}
+
+export {default as EnvDBHandler} from "./lib/EnvDBHandler"
